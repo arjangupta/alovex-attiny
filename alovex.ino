cb14709da57b8b39e98ca1f9bc1997ea09041138
Arjan Gupta <arjangupta95@gmail.com>
2022-06-10 01:51:19 -0500

    Start at 90% duty cycle and wait for 250ms at 0% duty cycle

--- alovex.ino
+++ alovex.ino
@@ -1,15 +1,17 @@
-const uint8_t pwm_pin = PIN4;
-const uint8_t BYTE_MAX = 255;
+const uint8_t PWM_PIN = PIN4;
+const uint8_t INITIAL_DUTY_CYCLE = 230; // Approx. 90% duty cycle
 
 void setup() {                
-  pinMode(pwm_pin, OUTPUT); // set the pin to output mode
+  pinMode(PWM_PIN, OUTPUT); // set the pin to output mode
 }
 
 void loop() {
-  uint8_t duty_cycle = BYTE_MAX; // Start 100% duty cycle, or fully illuminated
-  for (size_t i = 0; i < BYTE_MAX; i++)
+  uint8_t duty_cycle = INITIAL_DUTY_CYCLE; // initialize for count-down
+  for (uint8_t i = 0; i < INITIAL_DUTY_CYCLE; i++)
   {
-    analogWrite(pwm_pin, --duty_cycle);
-    delay(10);
+    analogWrite(PWM_PIN, --duty_cycle);
+    delay(15);
   }
+  // Wait for a small interval at the end
+  delay(250);
 }
