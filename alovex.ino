8bab53daf5bf9eadf7352e74129627a539efd395
Arjan Gupta <arjangupta95@gmail.com>
2022-06-10 01:36:23 -0500

    Implement 100 to 0 duty cycle decrease with 10ms delay

--- alovex.ino
+++ alovex.ino
@@ -1,14 +1,15 @@
-const int pwm_pin = PIN4;
+const uint8_t pwm_pin = PIN4;
+const uint8_t BYTE_MAX = 255;
 
 void setup() {                
   pinMode(pwm_pin, OUTPUT); // set the pin to output mode
 }
 
 void loop() {
-  analogWrite(pwm_pin, 255); // 100% duty cycle, or on
-  delay(1000);
-  analogWrite(pwm_pin, 128); // 50% duty cycle
-  delay(1000);
-  analogWrite(pwm_pin, 0);   // 0% duty cycle, or off
-  delay(1000);
+  uint8_t duty_cycle = BYTE_MAX; // Start 100% duty cycle, or fully illuminated
+  for (size_t i = 0; i < BYTE_MAX; i++)
+  {
+    analogWrite(pwm_pin, --duty_cycle);
+    delay(10);
+  }
 }
