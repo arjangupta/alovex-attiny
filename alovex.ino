9142b5ccdbc05864ded0631a10cc47720e37349b
Arjan Gupta <arjangupta95@gmail.com>
2022-06-10 01:03:04 -0500

    Only make pin1 blink

--- alovex.ino
+++ alovex.ino
@@ -1,13 +1,10 @@
 void setup() {                
-  pinMode(0, OUTPUT);      // LED on Model B
-  pinMode(1, OUTPUT);      // LED on Model A   
+  pinMode(PIN1, OUTPUT);      // on-board LED
 }
 
 void loop() {
-  digitalWrite(0, HIGH);   // Turn the LED on
-  digitalWrite(1, HIGH);
-  delay(1000);             // Wait for a second
-  digitalWrite(0, LOW);    // Turn the LED off
-  digitalWrite(1, LOW); 
-  delay(1000);             // Wait for a second
+  digitalWrite(PIN1, HIGH);
+  delay(1000); // wait 1 sec
+  digitalWrite(PIN1, LOW);
+  delay(1000); // wait 1 sec
 }
